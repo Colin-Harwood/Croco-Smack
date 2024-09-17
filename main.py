f74c3670718db536bacf34abc051910c7a1b2210
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-09-17 16:29:42 +0200

    initialised pygame components

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import sys
+
 import pygame
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Croco-smack')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
+
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
