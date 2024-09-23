dc54293e721d8b612b19d7983cf0f3ce0cac4f57
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-09-23 19:02:39 +0200

    Added images

--- scripts/util.py
+++ scripts/util.py
@@ -6,3 +6,11 @@ def load_image(path):
     img = pygame.image.load(BASE_IMG_PATH + path)
     img.set_colorkey((0, 0, 0))
     return img
+
+class Animation:
+    def __init__(self, images, img_dur, loop=True):
+        self.images = images
+        self.img_duration = img_dur
+        self.loop = loop
+        self.done = False
+        self.frame = 0
