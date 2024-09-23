2916b68df0176c8581ec2b52f0196aac34c4a724
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-09-23 17:31:48 +0200

    Added player movement

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from scripts.util import load_image
+from scripts.entities import PhysicsEntity
 
 class Game:
     def __init__(self):
@@ -13,18 +14,37 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
+        self.movement = [False, False]
+
         self.assets = {'background': load_image('background.png'),
                        'player': load_image('character/Soldier-Attack1.png')}
+        
+        self.player = PhysicsEntity(self, 'player', (0, 0), (15, 18))
 
     def run(self):
         while True:
             self.display.blit(self.assets['background'], (0, 0))
-            self.display.blit(self.assets['player'], (0, 0))
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+                        
                 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
---
+++ scripts/entities.py
@@ -0,0 +1,23 @@
+import pygame
+
+class PhysicsEntity:
+    def __init__(self, game, e_type, pos, size):
+        self.game = game
+        self.type = e_type
+        self.pos = list(pos)
+        self.size = size
+        self.velocity = [0, 0]
+        self.collisions = {'up': False, 'down':False, 'left': False, 'right': False}
+
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+    
+    def update(self, movement=(0, 0)):
+        entity_rect = self.rect()
+
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
+        self.pos[0] += movement[0]
+        self.pos[1] += self.velocity[1]
+
+    def render(self, surf, offset=(0, 0)):
+        surf.blit(self.game.assets['player'], (self.pos[0], self.pos[1]))
